f7a627c6c53e57a4875d2aef85e93fe2d2dbbeb5
Micael Santana <micael.santana@wisereducacao.com>
2022-07-29 18:54:25 -0300

    fix: now app module have the authmodule regardless.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -10,6 +12,8 @@ import configuration from './config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
