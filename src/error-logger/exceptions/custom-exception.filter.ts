1f433954bd0c6c544d3ba86f0df54d03fef97339
Micael Santana <micael.santana@wisereducacao.com>
2022-08-04 12:55:19 -0300

    feat: custom exception filter for 401 Unauthorized and not found 404 code.

---
+++ src/error-logger/exceptions/custom-exception.filter.ts
@@ -0,0 +1,18 @@
+import { ArgumentsHost, BadRequestException, Catch, HttpException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { ErrorLoggerService } from '../error-logger.service';
+import { BaseExceptionFilter } from "@nestjs/core";
+
+@Injectable()
+@Catch(Error, HttpException, BadRequestException)
+export class CustomExceptionFilter extends BaseExceptionFilter {
+  constructor(private errorLogger: ErrorLoggerService) {
+    super();
+  }
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (!(exception instanceof UnauthorizedException || exception instanceof NotFoundException)) {
+      this.errorLogger.log(exception);
+    }
+    super.catch(exception, host);
+  }
+}
