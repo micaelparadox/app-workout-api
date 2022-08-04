e5ef756e74363284c913fa893595068304cb342d
Micael Santana <micael.santana@wisereducacao.com>
2022-08-04 12:20:15 -0300

    feat: bootingstrap the app with swagger however.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gym Workout API')
+    .setDescription('Workout API')
+    .setVersion('1.0')
+    .addTag('gym')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
